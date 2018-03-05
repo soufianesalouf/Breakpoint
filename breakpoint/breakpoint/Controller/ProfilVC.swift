8e36216f5623f34b63a2d72749bac84ec922a186
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-05 23:27:45 +0000

    end lesson 9
    Building ProfilVC and Adding to UITabBarController

---
+++ breakpoint/breakpoint/Controller/ProfilVC.swift
@@ -0,0 +1,26 @@
+//
+//  ProfilVC.swift
+//  breakpoint
+//
+//  Created by Soufiane Salouf on 3/5/18.
+//  Copyright © 2018 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+class ProfilVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        //Sign out
+    }
+    
+}
