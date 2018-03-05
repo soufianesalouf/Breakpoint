04944f456b65d219884f6bffc4894afdd9df7c3c
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-05 19:56:45 +0000

    end lesson 5
    Creating InsetTextField and ShadowView Subclasses

---
+++ breakpoint/breakpoint/Utilities/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  breakpoint
+//
+//  Created by Soufiane Salouf on 3/5/18.
+//  Copyright © 2018 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+//Color
+let COLOR_PURPLE_PLACEHOLDER = #colorLiteral(red: 0.3254901961, green: 0.4215201139, blue: 0.7752227187, alpha: 0.5)
