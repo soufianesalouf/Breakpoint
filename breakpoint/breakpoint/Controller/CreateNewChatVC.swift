3bc75ec586408d7b90c55b5e4111d5268f20701f
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-06 05:35:17 +0000

    end lesson 15
    Creating CreateGroupVC and Connecting IBOutlets and Actions

---
+++ breakpoint/breakpoint/Controller/CreateNewChatVC.swift
@@ -0,0 +1,32 @@
+//
+//  CreateNewChatVC.swift
+//  breakpoint
+//
+//  Created by Soufiane Salouf on 3/6/18.
+//  Copyright © 2018 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+class CreateNewChatVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearshTextField: InsetTextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var doneBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
