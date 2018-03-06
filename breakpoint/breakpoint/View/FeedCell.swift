5fc6303b090cf15a5ae2d94499c2a6c02f38b2f0
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-06 01:08:21 +0000

    end lesson 12
    Building FeedCell

---
+++ breakpoint/breakpoint/View/FeedCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by Soufiane Salouf on 3/6/18.
+//  Copyright © 2018 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+    //outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String){
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+    
+
+    
+}
